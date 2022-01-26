f4f0f59edfcaf1bcc824a09204a76a8ad9258256
Jugalcody <jugalnerist@gmail.com>
2022-01-26 06:07:38 -0800

    google search engine

--- google.py
+++ google.py
@@ -38,10 +38,18 @@ class Google:  #Google class will create a google search engine
            m.bind("<Button>",lambda e: wb.open("https://www.google.co.in/imghp?hl=en-GB&tab=ri&ogbl"))
            
            email.bind("<Button>",lambda e: wb.open("https://mail.google.com/mail/u/0/"))
+           
            n=f'{os.getcwd()}/images.png'
-           a=tk.PhotoImage(file=n)
-           self.root.iconphoto("False",a)
-           self.root.mainloop()
+           #a=tk.PhotoImage(file=n)
+           #self.root.iconphoto("False",a)
+           try:
+              photo=tk.PhotoImage(file=(r''+str(n)))
+              self.root.iconphoto(False,photo)
+              
+           except:
+                 pass
+                
+           self.root.mainloop()   
 
 
 
