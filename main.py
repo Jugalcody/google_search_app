d0f5c7fbb2175ff0a924c181d826225a30bfe615
Jugalcody <jugalnerist@gmail.com>
2022-01-26 17:53:28 +0530

    main file

--- main.py
+++ main.py
@@ -4,19 +4,32 @@ def app():
  s=input("Have you installed python-pyinstaller(y/n)? ")
  if s=='y':
   try:
-   os.system("pyinstaller -i image.ico -F google.py")
+   os.system("pyinstaller -i images.ico -F google.py")
    a=os.getcwd()
    b=a+'/dist/google.exe'
-   os.system(f"cp {b} {a}")
-   os.system("rm -r build")
-   os.system("rm -r dist")
-   os.system("rm google.spec")
-   os.system("rm -r __pycache__")
-   os.system("rm gi*")
-   os.system("rm *.py")
-   os.system("rm image*")
-   os.system("rm LICENSE")
-   os.system("rm README.md")
+   os=input("you are using terminal or cmd(t/c)? ")
+   if os=='t':
+       os.system(f"cp {b} {a}")
+       os.system("rm -r build")
+       os.system("rm -r dist")
+       os.system("rm google.spec")
+       os.system("rm -r __pycache__")
+       os.system("rm gi*")
+       os.system("rm *.py")
+       os.system("rm image*")
+       os.system("rm LICENSE")
+       os.system("rm README.md")
+   elif os=='c':
+               os.system(f"copy {b} {a}")
+               os.system("rmdir /s build") 
+               os.system("rmdir /s dist")
+               os.system("del google.spec")
+               os.system("rmdir /s __pycache__")
+               os.system("del gi*")
+               os.system("del *.py")
+               os.system("del image*")
+               os.system("del LICENSE")
+               os.system("del README.md")
   except :
         print("an error occured, try agaian!!")
  else:
