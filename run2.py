8fc27f0b060d8e1257a6f9c70736f0c275612e69
Jugalcody <jugalnerist@gmail.com>
2022-01-26 20:51:25 -0800

    execute run2.py if using cmd

--- run2.py
+++ run2.py
@@ -1,6 +1,6 @@
 import os
-if(os.path.isfile('google')):
+if(os.path.isfile('google.exe')):
           os.system("google.exe")
 elif(os.path.isfile('main.py')):
-          os.system('python3 main.py')
+          os.system('python main.py')
 
